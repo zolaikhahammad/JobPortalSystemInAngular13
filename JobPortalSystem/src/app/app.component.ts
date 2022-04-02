257062bbe734d9d9e7875759c0be9a2e06ccb7b3
Zolaikha Hammad <70544823+zolaikhahammad@users.noreply.github.com>
2022-04-02 19:53:42 +0500

    Interpolation - 1
    Interpolation- To generate or view the dynamic data on the front either from a function or from an api call then interpolation is used. in the .ts file and viewed in the component.html file.
    
    We use the curly braces and name of the property to view the dynamic data {{propertyname}}
    
    Just to keep in mind we can not increment a number in the propertybeing called and cannot use the new keyword as well. We can do addition subtraction.

---
+++ JobPortalSystem/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  getTitle(){
+    return 'Nokri.com - Job Finder';
+  }
+}
